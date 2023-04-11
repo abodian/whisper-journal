6096464d2cef442661806906755a157b28026c7c
abodian <ajnbodian@gmail.com>
2023-04-11 18:13:03 +0100

    react navigation setup, user context included, home login signup components created

---
+++ frontend/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View, Button } from "react-native";
+
+export default function Home(props) {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Button title="Sign Out" />
+    </View>
+  );
+}
