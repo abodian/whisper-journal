1ec91cf5535b140d482097d88928793af1b33ed7
abodian <ajnbodian@gmail.com>
2023-04-11 19:37:16 +0100

    firebase auth implement, pre refactor

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDbL_FWcTUjPzUaBK-Zeh3veyLhyDkPb5s",
   authDomain: "whisper-journal-4ef93.firebaseapp.com",
@@ -16,6 +11,6 @@ const firebaseConfig = {
   measurementId: "G-DGXJYPT74B",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth();
+export default app;
