2c4acca75488bdf0e95ce2522c70f6c1e775245f
abodian <ajnbodian@gmail.com>
2023-04-11 23:56:03 +0100

    improved theme

---
+++ frontend/components/Button.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { Button as PaperButton } from 'react-native-paper'
+import { theme } from '../core/theme'
+
+export default function Button({ mode, style, ...props }) {
+  return (
+    <PaperButton
+      style={[
+        styles.button,
+        mode === 'outlined' && { backgroundColor: theme.colors.surface },
+        mode === 'contained' && { backgroundColor: theme.colors.primary},
+        style,
+      ]}
+      labelStyle={[
+        styles.text,
+        mode === 'outlined' && { color: '#000000' },
+      ]}
+      mode={mode}
+      {...props}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: '100%',
+    marginVertical: 10,
+    paddingVertical: 2,
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    lineHeight: 26,
+  },
+})
---
+++ frontend/screens/LoginScreen.js
@@ -0,0 +1,108 @@
+// Login
+import React, { useState } from "react";
+import { TouchableOpacity, StyleSheet, View } from "react-native";
+import { Text } from "react-native-paper";
+import Background from "../components/Background";
+import Logo from "../components/Logo";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import TextInput from "../components/TextInput";
+import BackButton from "../components/BackButton";
+import { theme } from "../core/theme";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+
+
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState({ value: "", error: "" });
+  const [password, setPassword] = useState({ value: "", error: "" });
+
+  const loginUser = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email.value, password.value);
+      navigation.reset({
+        index: 0,
+        routes: [{ name: "Dashboard" }],
+      });
+    } catch (error) {
+      let errorMessage = "";
+      if (
+        error.code === "auth/invalid-email" ||
+        error.code === "auth/wrong-password"
+      ) {
+        errorMessage = "Your email or password was incorrect";
+      } else if (error.code === "auth/email-already-in-use") {
+        errorMessage = "An account with this email already exists";
+      } else {
+        errorMessage = "There was a problem with your request";
+      }
+      setEmail({ ...email, error: errorMessage });
+      setPassword({ ...password, error: errorMessage });
+    }
+  };
+
+  return (
+    <Background>
+      <BackButton goBack={navigation.goBack} />
+      <Logo />
+      <Header>Welcome back.</Header>
+      <TextInput
+        label="Email"
+        returnKeyType="next"
+        value={email.value}
+        onChangeText={(text) => setEmail({ value: text, error: "" })}
+        error={!!email.error}
+        errorText={email.error}
+        autoCapitalize="none"
+        autoCompleteType="email"
+        textContentType="emailAddress"
+        keyboardType="email-address"
+      />
+      <TextInput
+        label="Password"
+        returnKeyType="done"
+        value={password.value}
+        onChangeText={(text) => setPassword({ value: text, error: "" })}
+        error={!!password.error}
+        errorText={password.error}
+        secureTextEntry
+      />
+      <View style={styles.forgotPassword}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("ResetPasswordScreen")}
+        >
+          <Text style={styles.forgot}>Forgot your password?</Text>
+        </TouchableOpacity>
+      </View>
+      <Button mode="contained" onPress={loginUser}>
+        Login
+      </Button>
+      <View style={styles.row}>
+        <Text>Don’t have an account? </Text>
+        <TouchableOpacity onPress={() => navigation.replace("RegisterScreen")}>
+          <Text style={styles.link}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+    </Background>
+  );
+}
+
+const styles = StyleSheet.create({
+  forgotPassword: {
+    width: "100%",
+    alignItems: "flex-end",
+    marginBottom: 24,
+  },
+  row: {
+    flexDirection: "row",
+    marginTop: 4,
+  },
+  forgot: {
+    fontSize: 13,
+    color: theme.colors.secondary,
+  },
+  link: {
+    fontWeight: "bold",
+    color: theme.colors.primary,
+  },
+});
