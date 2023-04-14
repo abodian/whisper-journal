e1c85d375934c6ed4f9ac424a8cde12697824841
MichalSmyk <michal.smyk38@gmail.com>
2023-04-14 16:41:23 +0100

    all issues fixed

--- frontend/components/AddEntry.js
+++ frontend/components/AddEntry.js
@@ -6,7 +6,7 @@ const AddEntry = ({ selectedDate, setSelectedDate }) => {
     const [input, setInput] = useState('');
  
 
-    const handleAddEntry = ({ selectedDate, setSelectedDate }) => {
+    const handleAddEntry = () => {
     const data = {
         title: title,
         input: input,
