6a45595a12d6325a5ab1eac52fdb34d776880943
MichalSmyk <michal.smyk38@gmail.com>
2023-04-14 15:31:46 +0100

    some styling

--- frontend/screens/SingleEntry.js
+++ frontend/screens/SingleEntry.js
@@ -1,10 +1,14 @@
 import { useRoute } from '@react-navigation/native';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Dimensions, Text } from 'react-native';
 import React, {useState} from 'react';
-import { Text } from 'react-native';
 import FormattedDate from '../components/Date';
 import AddEntry from '../components/AddEntry';
-import tw from 'react-native-tailwindcss';
+
+const { width, height } = Dimensions.get('window');
+const aspectRatio = width / height;
+const dateHeight = aspectRatio >= 0.75 ? height * 0.4 : height * 0.3;
+const addEntryHeight = (height - dateHeight) / 2;
+
 
 
 function SingleEntry() {
@@ -12,35 +16,34 @@ function SingleEntry() {
     const { date } = route.params;
     const [selectedDate, setSelectedDate] = useState(date);
 
-  return (
-    <View style={styles.container}>
-    <View style={styles.dateComponent}> 
-        <FormattedDate date={date} />
-    </View>
-     <View style={styles.entry}>
-        <AddEntry selectedDate={selectedDate} setSelectedDate={setSelectedDate}/>
-    </View>
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
+    return (
+      <View style={styles.container}>
+        <View style={styles.dateComponent}>
+          <Text>You are adding entry for <FormattedDate date={date} /></Text>
+        </View>
+        <View style={styles.entry}>
+          <AddEntry selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
+        </View>
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: 'center',
+      justifyContent: 'flex-start',
       alignItems: 'center',
     },
     dateComponent: {
-      flex: 1,
+      height: dateHeight,
       justifyContent: 'center',
       alignItems: 'center',
     },
     entry: {
-      flex: 1,
+      height: addEntryHeight,
       justifyContent: 'center',
       alignItems: 'center',
     },
   });
-  
 
 export default SingleEntry;
