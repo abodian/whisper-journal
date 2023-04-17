d0f23ab329a1de610e4df547020f3ed34d14f9d7
abodian <ajnbodian@gmail.com>
2023-04-17 17:42:19 +0100

    single entry has basic styling

--- frontend/screens/SingleEntry.js
+++ frontend/screens/SingleEntry.js
@@ -1,15 +1,15 @@
 import { useRoute } from '@react-navigation/native';
-import { View, StyleSheet, Dimensions, Text } from 'react-native';
+import { View, StyleSheet, Dimensions, Text, KeyboardAvoidingView } from 'react-native';
 import React, {useState} from 'react';
 import FormattedDate from '../components/Date';
 import AddEntry from '../components/AddEntry';
+import BackButton from "../components/BackButton";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const { width, height } = Dimensions.get('window');
 const aspectRatio = width / height;
 const dateHeight = aspectRatio >= 0.75 ? height * 0.4 : height * 0.3;
 const addEntryHeight = (height - dateHeight) / 2;
-
-
 
 function SingleEntry({navigation}) {
     const route = useRoute();
@@ -17,14 +17,23 @@ function SingleEntry({navigation}) {
     const [selectedDate, setSelectedDate] = useState(date);
 
     return (
-      <View style={styles.container}>
-        <View style={styles.dateComponent}>
-          <Text>You are adding entry for <FormattedDate date={date} /></Text>
-        </View>
-        <View style={styles.entry}>
+      <KeyboardAvoidingView style={styles.container} behavior='padding'>
+        <BackButton goBack={navigation.goBack} />
+        <View style={styles.userInputContainer}>
+          <Text style={{borderWidth: 0.5, borderColor: 'gray', padding: 15, borderRadius: 10, marginTop: 60, marginBottom: 20}}>You are adding entry for <FormattedDate date={date} /></Text>
+          <Text >Press the microphone button to add your entry!</Text>
           <AddEntry selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
         </View>
-      </View>
+        <View style={styles.entry}>
+          <Icon.Button
+            name="microphone" 
+            size={150}
+            color='black'
+            backgroundColor="transparent"
+            // onPress={}  
+          />
+        </View>
+      </KeyboardAvoidingView>
     );
   }
   
@@ -34,15 +43,18 @@ function SingleEntry({navigation}) {
       justifyContent: 'flex-start',
       alignItems: 'center',
     },
-    dateComponent: {
+    userInputContainer: {
+      flex: 3,
+      justifyContent: 'flex-start',
       height: dateHeight,
-      justifyContent: 'center',
       alignItems: 'center',
-    },
+    },    
     entry: {
-      height: addEntryHeight,
-      justifyContent: 'center',
+      flex: 1,
+      justifyContent: 'flex-start',
       alignItems: 'center',
+      justifyContent: 'center',
+      marginBottom: 40
     },
   });
 
