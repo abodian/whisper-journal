181aa7b4e7f1a62204cb02291face41000de3f8f
abodian <ajnbodian@gmail.com>
2023-04-19 12:18:20 +0100

    communicating with 3.5-turbo from server

---
+++ backend/openai/OpenAI.js
@@ -0,0 +1,30 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+class OpenAI {
+  constructor(apiKey) {
+    // Create the Configuration and OpenAIApi instances
+    this.openai = new OpenAIApi(
+      new Configuration("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")
+    );
+  }
+  // Asynchronous function to generate text from the OpenAI API
+  async generateText(prompt, model, max_tokens, temperature = 0.85) {
+    try {
+      // Send a request to the OpenAI API to generate text
+      const response = await this.openai.createCompletion({
+        model,
+        prompt,
+        max_tokens,
+        n: 1,
+        temperature,
+      });
+      console.log(`request cost: ${response.data.usage.total_tokens} tokens`);
+      // Return the text of the response
+      return response.data.choices[0].text;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+module.exports = { OpenAI };
