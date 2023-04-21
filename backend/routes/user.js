d3926e63854bd6cc849e8ed08c8b181186d12fdc
abodian <ajnbodian@gmail.com>
2023-04-21 18:01:15 +0100

    account creation adds both mongodb user and firebase user with same id

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const UsersController = require('../controllers/user');
+const UsersController = require("../controllers/user");
 
-router.post('/', UsersController.Create);
+router.post("/", UsersController.Create);
+// router.post("/firebase", UsersController.FirebaseUserCreation);
 
 module.exports = router;
