08e5f4ccb356b29bc3d42bd4bf78c9e74533329a
MichalSmyk <michal.smyk38@gmail.com>
2023-04-24 16:35:16 +0100

    update schema

--- backend/models/analyse.js
+++ backend/models/analyse.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
 const AnalysisSchema = new mongoose.Schema({
-  entryId: { type: mongoose.Schema.Types.ObjectId, required: false, ref: 'Entry' },
+  entryId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Entry' },
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
   result: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
 
+
 const Analysis = mongoose.model('Analysis', AnalysisSchema, 'Analysis');
+
 
 module.exports = Analysis;
