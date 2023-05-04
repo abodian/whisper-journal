c103f613d4ec986e86b36c2e49a7d99f65d580c4
MichalSmyk <michal.smyk38@gmail.com>
2023-05-04 14:14:25 +0100

    mongo id not being added as desired

--- frontend/core/AddEntry.js
+++ frontend/core/AddEntry.js
@@ -30,7 +30,7 @@ const AddEntry = ({ selectedDate, transcription }) => {
     };
 // 192.168.1.197 //mike
     // fetch('http://192.168.0.106:3001/entry', { // alex
-    fetch('https://whisper-journal1.onrender.com/entry', {
+    fetch('http://192.168.1.197:3001/entry', {
         method: 'POST',
         headers: {
         'Content-Type': 'application/json',
